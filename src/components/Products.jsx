f26461c4d7da5815381d60b21ed4f92d25937832
alikurniawan <alikoerniawan02@gmail.com>
2020-10-07 11:04:28 +0700

    update

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -46,7 +46,7 @@ class Products extends Component {
           KodeBarang: "MDS-BK01",
           NamaBarang: "Madu Hitam",
           Ukuran: "250 ml",
-          Harga: "Rp. 60,000,-",
+          Harga: "Rp. 65,000,-",
           Deskripsi: "Madu Hitam Sumbawa Botol Kecil",
         },
         {
@@ -54,7 +54,7 @@ class Products extends Component {
           KodeBarang: "MDS-BK02",
           NamaBarang: "Madu Putih",
           Ukuran: "250 ml",
-          Harga: "Rp. 60,000,-",
+          Harga: "Rp. 65,000,-",
           Deskripsi: "Madu Putih Sumbawa Botol Kecil",
         },
         {
@@ -62,12 +62,12 @@ class Products extends Component {
           KodeBarang: "MDS-BK03",
           NamaBarang: "Madu Manis",
           Ukuran: "250 ml",
-          Harga: "Rp. 60,000,-",
+          Harga: "Rp. 65,000,-",
           Deskripsi: "Madu Manis Sumbawa Botol Kecil",
         },
         {
           Image: `${mdsbb04}`,
-          KodeBarang: "MDS-BK03",
+          KodeBarang: "MDS-BB04",
           NamaBarang: "Madu Hutan",
           Ukuran: "760 ml",
           Harga: "Rp. 210,000,-",
